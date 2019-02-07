dd9264ccfbae994d6e4294e925606df6b4e8508d
Your Name <you@example.com>
2019-02-07 18:07:43 +0800

    banner 查询

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,4 +1,13 @@
+
+
 $(function(){
+  var pageNum =1;
+  var pageSize = 2;
+
+  //默认调用search
+  search(pageNum,pageSize);
+
+
   // alert(1);
   $('#bannerAdd').click(function(){
     $.post('/banner/add',{
@@ -8,16 +17,57 @@ $(function(){
       console.log(res);
       if(res.code === 0){
         //成功
+        layer.msg('添加成功');
       }else{
         //ps:很多时候，真正的错误信息不会给用户去看
         console.log(res.msg);
-        alert('网络异常，请稍后重试');
+        layer.msg('网络异常，请稍后重试');
       }
           //手动调用关闭的方法
       $('#myModal').modal('hide');
+      //手动清空输入框的内容
+      $('#inputEmail3').val('');
+      $('#inputPassword3').val('');
     })
-    // alert('ajax提交');
-
-
   })
 })
+
+
+
+
+/**
+ * 查询banner数据的方法
+ */
+function search(pageNum,pageSize){
+  $.get('/banner/search',{
+    pageNum:pageNum,
+    pageSize:pageSize
+  },function(result){
+    if(result.code === 0){
+      layer.msg('查询成功');
+
+      for(var i = 0;i < result.data.length;i++){
+        var item = result.data[i];
+        $('#banner-table tbody').append(
+          `
+            <tr>
+              <td>${item._id}</td>
+              <td>${item.name}</td>
+              <td>
+                <img id="banner-img" src="${item.imgUrl}"
+                alt="">
+              </td>
+              <td>
+                <a href="javascript:;">删除</a>
+                <a href="javascript:;">修改</a>
+              </td>
+            </tr>
+          `
+        )
+      }
+    }else {
+      console.log(result.msg);
+      layer.msg('网络异常，请稍后重试');
+    }
+  })
+}
--- routes/banner.js
+++ routes/banner.js
@@ -1,29 +1,75 @@
 //提供给前端 ajax调用的 接口地址 url
 const express = require('express');
+const async = require('async');
 const BannerModel = require('../models/banner');
 const router = express.Router();
 
 //首页 - http://localhost:3000/banner/add
-router.post('/add',(req,res) => {
+router.post('/add', (req, res) => {
   //获取前端传递过来的参数
-  var banner =  new BannerModel({
-    name:req.body.bannerName,
-    imgUrl:req.body.bannerUrl
+  var banner = new BannerModel({
+    name: req.body.bannerName,
+    imgUrl: req.body.bannerUrl
   })
-  banner.save(function(err){
-    if(err){
+  banner.save(function (err) {
+    if (err) {
       res.json({
-        code:-1,
-        msg:err.message
+        code: -1,
+        msg: err.message
       })
-    }else{
+    } else {
       //成功
       res.json({
-        code:0,
-        msg:'ok'
+        code: 0,
+        msg: 'ok'
       })
     }
   })
 });
 
+//搜索or查询banner-   http://localhost:3000/banner/search
+router.get('/search', (req, res) => {
+  //分页
+  //1、得到前端传递过来的参数
+  let pageNum = parseInt(req.query.pageNum) || 1;  //当前的页数
+  let pageSize = parseInt(req.query.pageSize) || 2; //每页显示的条数
+  
+
+  //采用并行无关联
+  async.parallel([
+    function(cb){
+      BannerModel.find().count().then(num=> {
+        totalSize = num;
+        cb(null,num);
+      }).catch(err =>{
+        cb(err);
+      })
+    },
+
+    function(cb){
+      BannerModel.find().skip(pageNum * pageSize - pageSize).limit(pageSize)
+      .then(data => {
+        cb(null,data);
+      }).catch(err =>{
+        cb(err);
+      })
+    }
+  ],function(err,result){
+    console.log(result);
+      if(err){
+        res.json({
+          code:-1,
+          msg:err.message
+        })
+      }else{
+        res.json({
+          code:0,
+          msg:'ok',
+          data: result[1],
+          totalPage:Math.ceil(result[0]/pageSize),
+          totalSize:result[0]
+        })
+      }
+  }) 
+})
 module.exports = router;
